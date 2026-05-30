a0166edd494cee747175a17c55b6bd83cf49ad3d
Devesh <deveshchauriwar50@.com>
2026-05-30 04:12:20 +0000

    chore: configured production proxy for vercel connectivity

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://peppy-we0g.onrender.com', // Tumhara live Render backend link
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
